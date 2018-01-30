c2d3f138f79f6ebd666b068ea64a7ef8bd2083bf
phatt1889 <truongthanhphat89@gmail.com>
2018-01-30 21:33:07 +0700

    new

---
+++ TradeCoin/src/main/java/com/ttp/TradeCoin/TradeCoinApplication.java
@@ -0,0 +1,12 @@
+package com.ttp.TradeCoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeCoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeCoinApplication.class, args);
+	}
+}
---
+++ TradeCoin/src/test/java/com/ttp/TradeCoin/TradeCoinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ttp.TradeCoin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TradeCoinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
